f2a33c6d5177419ace5bad5533e1dcaac6bd71d5
mutasim-abuhlal <mutasimcoc7@gmail.com>
2026-07-19 13:45:46 +0300

    Add files via upload

---
+++ UtilityLib/clsUtility.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UtilityLib
+{
+    public class clsUtility
+    {
+        public static void WriteEventLogEntry(string SourceName, string Message, EventLogEntryType type)
+        {
+            if (!EventLog.SourceExists(SourceName,"."))
+            {
+                EventLog.CreateEventSource(SourceName, "Application");
+            }
+
+            EventLog.WriteEntry(SourceName, Message, type);
+        }
+
+        public static string PerformHash(string Input)
+        {
+            using (SHA256 sHA256 = SHA256.Create())
+            {
+                byte[] HashedBytes = sHA256.ComputeHash(Encoding.UTF8.GetBytes(Input));
+
+                return BitConverter.ToString(HashedBytes).Replace("-", "").ToLower();
+            }
+        }
+
+        public static string Encrypt(string plainText, string key)
+        {
+            using (Aes aesAlg = Aes.Create())
+            {
+                aesAlg.Key = Encoding.UTF8.GetBytes(key);
+                aesAlg.IV = new byte[aesAlg.BlockSize / 8];
+
+                ICryptoTransform encryptor = aesAlg.CreateEncryptor(aesAlg.Key, aesAlg.IV);
+
+                using (MemoryStream msEncrypt = new MemoryStream())
+                {
+                    using (CryptoStream csEncrypt = new CryptoStream(msEncrypt, encryptor, CryptoStreamMode.Write))
+                    using (StreamWriter swEncrypt = new StreamWriter(csEncrypt))
+                    {
+                        swEncrypt.Write(plainText);
+                    }
+
+                    return Convert.ToBase64String(msEncrypt.ToArray());
+                }
+            }
+        }
+
+        public static string Decrypt(string cipherText, string key)
+        {
+            using (Aes aesAlg = Aes.Create())
+            {
+                aesAlg.Key = Encoding.UTF8.GetBytes(key);
+                aesAlg.IV = new byte[aesAlg.BlockSize / 8];
+
+                ICryptoTransform decryptor = aesAlg.CreateDecryptor(aesAlg.Key, aesAlg.IV);
+
+                using (MemoryStream msDecrypt = new MemoryStream(Convert.FromBase64String(cipherText)))
+                using (CryptoStream csDecrypt = new CryptoStream(msDecrypt, decryptor, CryptoStreamMode.Read))
+                using (StreamReader srDecrypt = new StreamReader(csDecrypt))
+                {
+                    return srDecrypt.ReadToEnd();
+                }
+            }
+        }
+    }
+}
